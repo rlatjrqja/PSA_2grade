9b3474fe9132478ddc3e64afb85d3dcd359d26f0
rlatjrqjaLaptop <rlatjrqja@yuhan.ac.kr>
2023-10-04 13:37:27 +0900

    문제해결능력 5주차 백업

---
+++ week05/week05/Program.cs
@@ -0,0 +1,17 @@
+﻿String path = "C:\\MyFiles\\yuhan\\PSA\\PSA_2grade\\week05\\";
+
+Console.Write("파일명 : ");
+String fileName = Console.ReadLine();
+
+String TargetFile = "ReadTestFile.txt";
+StreamReader sr1 = new StreamReader(path + TargetFile);
+
+while (sr1.EndOfStream == false)
+{
+    string s = sr1.Read().ToString();
+    Console.Write(s);
+}
+
+StreamWriter sw1 = new StreamWriter(path + fileName);
+sw1.WriteLine(sr1);
+sw1.Close();
