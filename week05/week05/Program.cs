ca3199e68c9c68fff5ff47b7b462eb4b5eeb52dd
rlatjrqjaUniv <rlatjrqja@yuhan.ac.kr>
2023-10-07 22:53:19 +0900

    디렉토리 생성부분 수정

--- week05/week05/Program.cs
+++ week05/week05/Program.cs
@@ -2,6 +2,16 @@
 Console.Write("경로를 지정하세요 : ");
 String dir = Console.ReadLine();
 
+DirectoryInfo dirInfo = new DirectoryInfo(dir);
+if(dirInfo.Exists==false)
+{
+    Console.WriteLine("존재하지 않는 경로입니다. 입력한 경로를 생성합니다.");
+    dirInfo.Create();
+}//인스턴스 매서드. 객체 지향적 방법
+
+//Directory.CreateDirectory(dir);
+//정적 메서드
+
 //string dir = Environment.CurrentDirectory;
 //week05.exe의 위치(실행위치)를 가져온다???
 
@@ -26,6 +36,7 @@ sw.Close();
 Console.WriteLine($"{dir + "\\" + filename}에 작성되었습니다.\n");
 
 StreamReader sr = new StreamReader(dir + "\\" + filename);
+Console.WriteLine($"{filename}내용");
 while (true)
 {
     output = sr.ReadLine();
